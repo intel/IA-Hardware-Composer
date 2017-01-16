kalyankondapally/hwcomposer

Initial import.

common/compositor/gl/glprogram.h
@@ -1,0 +1,51 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#ifndef GL_PROGRAM_H_
+#define GL_PROGRAM_H_
+
+#include <vector>
+
+#include "glscopedtypes.h"
+
+namespace hwcomposer {
+
+struct RenderState;
+
+class GLProgram {
+ public:
+  GLProgram();
+  GLProgram(const GLProgram& rhs) = delete;
+  GLProgram& operator=(const GLProgram& rhs) = delete;
+
+  ~GLProgram();
+
+  bool Init(unsigned texture_count);
+  void UseProgram(const RenderState& cmd, GLuint viewport_width,
+                  GLuint viewport_height);
+
+ private:
+  ScopedGLProgram program_;
+  GLint viewport_loc_;
+  GLint crop_loc_;
+  GLint alpha_loc_;
+  GLint premult_loc_;
+  GLint tex_matrix_loc_;
+  bool initialized_;
+};
+
+}  // namespace hwcomposer
+#endif  // GL_PROGRAM_H_

intel/IA-Hardware-Composer

Includes Several commits:
commit aee43ab680941f5c7322cd62abb91b1ddf398852
Author: Kalyan Kondapally <kalyan.kondapally@intel.com>
Date:   Sat Jan 14 23:35:35 2017 -0800
    Fix issues related to Tests.
    This fixes the linking issues we had with some of the GL
    and EGL API. Test apps build and link but Atomic commit fails
    during run time.
    Jira: None.
    Test: Builds on Linux and Android. No regressions seen on Android.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>
commit 8ff76e215cfdb08a92382a11ea15ac3188ddf6fa
Date:   Sat Jan 14 16:47:26 2017 -0800
    Use sync wait instead of waiting in a loop to submit commit.
    Inspired by: https://github.com/rantogno/kmscube/commit/fc52268da79ed406da02c88f849f1ef70dab789f
    Test: Builds and boots to home screen. No Graphics regrssion.
commit 3aa1ae042b9ccd7c4f768db0498a056b92903f71
Author: Johnson Lin <johnson.lin@intel.com>
Date:   Thu Jan 12 10:58:08 2017 +0800
    Add kmscube as test program
    Test program doesn't link yet. Imported from
    https://github.com/rantogno/kmscube
    Test: None.
commit 0f7cfbc9bc8fcec3e296433ac9d09e549cc4967c
Author: Tapani Pälli <tapani.palli@intel.com>
Date:   Tue Jan 10 06:58:27 2017 -0800
    hwcomposer: in case of EBUSY, spin until commit succeeded
    Bug: #11
    Test: AnTuTu Marooned demo works correctly
    Signed-off-by: Tapani Pälli <tapani.palli@intel.com>
Initial Linux support added by Johnson Lin <johnson.lin@intel.com>

common/compositor/gl/glprogram.h
@@ -1,0 +1,51 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#ifndef GL_PROGRAM_H_
+#define GL_PROGRAM_H_
+
+#include <vector>
+
+#include "shim.h"
+
+namespace hwcomposer {
+
+struct RenderState;
+
+class GLProgram {
+ public:
+  GLProgram();
+  GLProgram(const GLProgram& rhs) = delete;
+  GLProgram& operator=(const GLProgram& rhs) = delete;
+
+  ~GLProgram();
+
+  bool Init(unsigned texture_count);
+  void UseProgram(const RenderState& cmd, GLuint viewport_width,
+                  GLuint viewport_height);
+
+ private:
+  GLint program_ = 0;
+  GLint viewport_loc_;
+  GLint crop_loc_;
+  GLint alpha_loc_;
+  GLint premult_loc_;
+  GLint tex_matrix_loc_;
+  bool initialized_;
+};
+
+}  // namespace hwcomposer
+#endif  // GL_PROGRAM_H_