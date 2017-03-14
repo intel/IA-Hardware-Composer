02608b13e950f67d030e5e1fcd55bfc5761b6f95
Rafael Antognolli <rafael.antognolli@intel.com>
2017-03-14 08:30:21 -0700

    Add documentation to FDHandler.
    Jira: None.
    Test: None.
    
    Signed-off-by: Rafael Antognolli <rafael.antognolli@intel.com>

--- common/utils/fdhandler.h
+++ common/utils/fdhandler.h
@@ -21,17 +21,35 @@
 
 namespace hwcomposer {
 
+// Class wrapper around poll() and struct pollfd.
+// It keeps the fds added on it so one doesn't have to keep
+// recreating/populating the pollfd manually.
 class FDHandler {
  public:
   FDHandler();
   virtual ~FDHandler();
 
-  // Add fd to the list of fds that we care about
+  // Add fd to the list of fds that we care about. This makes ::Poll watch for
+  // this fd when called.
   bool AddFd(int fd);
+
+  // Remove the fd from the list of fds that we are watching.
   bool RemoveFd(int fd);
 
+  // Call poll() on the list of fds that we are watching. Will block if
+  // timemout > 0. Store the result from the poll request, so it can be queried
+  // with ::IsReady().
+  //  - timeout: time in miliseconds to stay blocked before returning if no fd
+  //  is ready.
+  //  - return: number of fds ready. If return is 0, it means we timed out. If
+  //  return is < 0, an error has ocurred.
   int Poll(int timeout);
 
+  // Check whether this fd is ready.
+  // - return: 1 if fd is ready to read
+  //           0 if fd is not ready
+  //           -1 if there's an error on the fd
+  //           -2 if the fd is closed and can't be polled
   int IsReady(int fd) const;
 
  private:
