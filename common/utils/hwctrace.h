fa98737b6a58433bbf003f425e527ed3b8aa4d0c
Pallavi G <pallavi.g@intel.com>
2017-03-31 13:39:12 +0530

    Build fix with display dump enable
    Jira: None
    Test: Build Passed with Display dump enable
    
    Signed-off-by: Pallavi G <pallavi.g@intel.com>

--- common/utils/hwctrace.h
+++ common/utils/hwctrace.h
@@ -164,7 +164,7 @@ class TraceFunc {
       "Dumping DisplayPlaneState of Current Composition planes "           \
       "-----------------------------");                                    \
   DUMPTRACE("Frame: %d", frame_);                                          \
-  DUMPTRACE("Total Layers for this Frame: %d", queue_item.layers_.size()); \
+  DUMPTRACE("Total Layers for this Frame: %d", layers.size()); \
   DUMPTRACE("Total Planes in use for this Frame: %d",                      \
             current_composition_planes.size());                            \
   int plane_index = 1;                                                     \
@@ -178,7 +178,7 @@ class TraceFunc {
         DUMPTRACE("Layers Index:");                                        \
         for (size_t primary_index : source_layers) {                       \
           DUMPTRACE("index: %d", primary_index);                           \
-          queue_item.layers_.at(primary_index).Dump();                     \
+          layers.at(primary_index).Dump();                     \
         }                                                                  \
         break;                                                             \
       case DisplayPlaneState::State::kScanout:                             \
@@ -191,7 +191,7 @@ class TraceFunc {
         DUMPTRACE("Layers Index:");                                        \
         for (size_t overlay_index : source_layers) {                       \
           DUMPTRACE("index: %d", overlay_index);                           \
-          queue_item.layers_.at(overlay_index).Dump();                     \
+          layers.at(overlay_index).Dump();                     \
         }                                                                  \
         break;                                                             \
       default:                                                             \
