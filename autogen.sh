b2cf4e332872bd21572e555cd8ee1cf01fc08b43
Harish Krupo <harish.krupo.kps@intel.com>
2017-11-18 02:17:05 +0530

    Linux Backend Support
    Jira: None.
    Test: Build passes on Linux.
    
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

--- autogen.sh
+++ autogen.sh
@@ -8,13 +8,6 @@ cd $srcdir
 
 mkdir -p m4
 
-if ! type gtkdocize > /dev/null 2>&1; then
-        echo "EXTRA_DIST =" > gtk-doc.make
-        echo "CLEANFILES =" >> gtk-doc.make
-else
-        gtkdocize || exit $?
-fi
-
 autoreconf -v --install -Itests/third_party/json-c/autoconf-archive/m4 || exit 1
 cd $ORIGDIR || exit $?
 
