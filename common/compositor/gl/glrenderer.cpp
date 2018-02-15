e70d5373edcae6af73b1be6222082d1d74240104
Kalyan Kondapally <kalyan.kondapally@intel.com>
2018-02-14 22:44:23 -0800

    Use displayframe as scissor rect for full clear.
    Jira: None.
    Test: Android launches to home screen.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- common/compositor/gl/glrenderer.cpp
+++ common/compositor/gl/glrenderer.cpp
@@ -103,18 +103,27 @@ bool GLRenderer::Draw(const std::vector<RenderState> &render_states,
 
   glViewport(left, top, frame_width, frame_height);
 
-  if (clear_surface || partial_clear) {
-    const HwcRect<int> &damage = surface->GetSurfaceDamage();
-    GLuint clear_width = damage.right - damage.left;
-    GLuint clear_height = damage.bottom - damage.top;
-    if ((frame_width != clear_width) || (frame_height != clear_height)) {
-      glEnable(GL_SCISSOR_TEST);
-      glScissor(damage.left, damage.top, clear_width, clear_height);
-      glClear(GL_COLOR_BUFFER_BIT);
+  if (partial_clear || clear_surface) {
+    GLuint clear_width = 0;
+    GLuint clear_height = 0;
+    GLuint vleft = 0;
+    GLuint vtop = 0;
+    if (partial_clear) {
+      const HwcRect<int> &damage = surface->GetSurfaceDamage();
+      clear_width = damage.right - damage.left;
+      clear_height = damage.bottom - damage.top;
+      vleft = damage.left;
+      vtop = damage.top;
     } else {
-      glClear(GL_COLOR_BUFFER_BIT);
-      glEnable(GL_SCISSOR_TEST);
+      const HwcRect<int> &display_rect = surface->GetLayer()->GetDisplayFrame();
+      clear_width = display_rect.right - display_rect.left;
+      clear_height = display_rect.bottom - display_rect.top;
+      vleft = display_rect.left;
+      vtop = display_rect.top;
     }
+    glEnable(GL_SCISSOR_TEST);
+    glScissor(vleft, vtop, clear_width, clear_height);
+    glClear(GL_COLOR_BUFFER_BIT);
   } else {
     glEnable(GL_SCISSOR_TEST);
   }
