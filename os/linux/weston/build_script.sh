17d971d82d3cdacad90bc8cfb7827b93ec15744c
Harish Krupo <harishkrupo@gmail.com>
2018-02-18 10:09:02 +0530

    Build linux frontend using travis-ci
    Add required scripts to build linux frontend as a part of travis.
    
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- os/linux/weston/build_script.sh
+++ os/linux/weston/build_script.sh
@@ -3,9 +3,16 @@
 # This script assumes that it is run from the root
 # of the weston directory and also that hwcomposer
 # is cloned inside the root of weston's dir as iahwc
+# these variables can be changed using --weston-dir and
+# --iahwc-dir options to the script. see --help.
+
+function print_help() {
+    #fill in help
+    echo "HELP"
+    exit;
+}
 
 function build_iahwc() {
-
     # Build IAHWC
     pushd . > /dev/null
     cd iahwc
@@ -25,16 +32,12 @@ function build_weston() {
 
 BKR=71c4f70e08faad6002ec8fe8cd1c7930bee8373b
 AUTOGEN_CMDLINE=""
-PATCHES="iahwc/os/linux/weston/patches/*"
 APPLY_PATCHES=0;
 BUILD_IAHWC=0
 BUILD_WESTON=0
 PARALLEL=9
-
-if [ ! -z ${WLD+x} ]; then
-    echo "WLD found: $WLD"
-    AUTOGEN_CMDLINE="--prefix=$WLD"
-fi
+WESTON_DIR="$(pwd)"
+IAHWC_DIR="iahwc"
 
 for i in $@; do
     case $i in
@@ -51,16 +54,39 @@ for i in $@; do
         --build-weston)
             BUILD_WESTON=1;
             ;;
+        --weston-dir=*)
+            WESTON_DIR="${i//*=}";
+            ;;
+        --iahwc-dir=*)
+            IAHWC_DIR="${i//*=}";
+            ;;
         -j=* | --parallel=*)
             PARALLEL="${i//*=}";
             ;;
+        -h | --help | *)
+            print_help;
     esac
 done
 
+if [ ! -z ${WLD+x} ]; then
+    echo "WLD found: $WLD"
+    AUTOGEN_CMDLINE="--prefix=$WLD"
+fi
+
+cd $WESTON_DIR
+
+PATCHES="$IAHWC_DIR/os/linux/weston/patches/*"
 # checkout to weston best known revision and apply patches
 if [ $APPLY_PATCHES -eq 1 ]; then
     git checkout $BKR
-    git apply --check $PATCHES && git am $PATCHES
+    sed -i "s/IAHWC_DIR/$IAHWC_DIR/" $PATCHES
+    git am $PATCHES
+
+    # reset patches to use IAHWC_DIR.
+    pushd . > /dev/null
+    cd $IAHWC_DIR
+    git checkout os/linux/weston/patches
+    popd
 fi
 
 [ $BUILD_IAHWC -eq 1 ] && build_iahwc;
