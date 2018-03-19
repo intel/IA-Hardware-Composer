44f76a6a1780f64534a7c1e886ea6e473550f01b
Kalyan Kondapally <kalyan.kondapally@intel.com>
2018-03-19 00:20:08 -0700

    Ensure we swap Media surface as needed.
    Jira: None.
    Test: Video playback works fine.
    
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- common/compositor/compositor.cpp
+++ common/compositor/compositor.cpp
@@ -83,6 +83,7 @@ bool Compositor::Draw(DisplayPlaneStateList &comp_planes,
                               plane.GetSourceLayers().begin(),
                               plane.GetSourceLayers().end());
       media_state.emplace_back();
+      plane.SwapSurfaceIfNeeded();
       DrawState &state = media_state.back();
       state.surface_ = plane.GetOffScreenTarget();
       MediaState &media_state = state.media_state_;
@@ -93,7 +94,6 @@ bool Compositor::Draw(DisplayPlaneStateList &comp_planes,
       lock_.unlock();
       const OverlayLayer &layer = layers[plane.GetSourceLayers().at(0)];
       media_state.layer_ = &layer;
-      plane.SwapSurfaceIfNeeded();
     } else if (plane.NeedsOffScreenComposition()) {
       comp = &plane;
       plane.SwapSurfaceIfNeeded();
