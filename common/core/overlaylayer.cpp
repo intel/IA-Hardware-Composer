intel/IA-Hardware-Composer

commit fb2fdd91d3cb99adcb1737131b3f55b69a0bf6ca

Handle Surface Damage correctly in case of Transforms.
We didn't flip the SurfaceDamage co-ordinates in this case
like RenderState does with Display Frame and Source Rects.
Let's handle 90 and 270 case for SurfaceDamage.

Jira: #278.
Test: Rendering works fine when displays are rotated 90/270.
Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

common/core/overlaylayer.cpp
@@ -233,13 +233,16 @@ void OverlayLayer::InitializeState(HwcLayer* layer,
   }
 
   surface_damage_ = layer->GetLayerDamage();
-  // In case of layer using blening we need to force partial clear. Otherwise
-  // we see content not getting updated correctly. For example:
-  // on Android enable, settings put system user_rotation 1 and
-  // navigate to settings on Android.
-  if (((blending_ != HWCBlending::kBlendingNone) && !surface_damage_.empty())) {
-    state_ |= kForcePartialClear;
-    surface_damage_ = layer->GetDisplayFrame();
+
+  switch (plane_transform_) {
+    case HWCTransform::kTransform270:
+    case HWCTransform::kTransform90: {
+      std::swap(surface_damage_.left, surface_damage_.top);
+      std::swap(surface_damage_.right, surface_damage_.bottom);
+      break;
+    }
+    default:
+      break;
   }
 
   SetBuffer(layer->GetNativeHandle(), layer->GetAcquireFence(),

common/display/displayqueue.cpp
@@ -264,7 +264,6 @@ void DisplayQueue::GetCachedLayers(const std::vector<OverlayLayer>& layers,
       HwcRect<int> surface_damage = HwcRect<int>(0, 0, 0, 0);
       bool update_rect = reset_plane;
       bool refresh_surfaces = reset_composition_regions;
-      bool force_partial_clear = false;
 
       const std::vector<size_t>& source_layers = target_plane.GetSourceLayers();
       size_t layers_size = source_layers.size();
@@ -279,10 +278,6 @@ void DisplayQueue::GetCachedLayers(const std::vector<OverlayLayer>& layers,
 
           if (refresh_surfaces) {
             continue;
-          }
-
-          if (layer.NeedsPartialClear()) {
-            force_partial_clear = true;
           }
 
           if (layer.HasLayerContentChanged()) {
@@ -329,21 +324,14 @@ void DisplayQueue::GetCachedLayers(const std::vector<OverlayLayer>& layers,
         }
       }
 
-      if (update_rect || refresh_surfaces || !surface_damage.empty() ||
-          force_partial_clear) {
+      if (update_rect || refresh_surfaces || !surface_damage.empty()) {
         needs_gpu_composition = true;
         if (target_plane.NeedsSurfaceAllocation()) {
           display_plane_manager_->SetOffScreenPlaneTarget(target_plane);
         } else if (refresh_surfaces || reset_plane) {
           target_plane.RefreshSurfaces(NativeSurface::kFullClear, true);
-        } else if (!update_rect) {
-          if (force_partial_clear) {
-            target_plane.RefreshSurfaces(NativeSurface::kPartialClear, true);
-          }
-
-          if (!surface_damage.empty()) {
-            target_plane.UpdateDamage(surface_damage);
-          }
+        } else if (!update_rect && !surface_damage.empty()) {
+          target_plane.UpdateDamage(surface_damage);
         }
 
         if (refresh_surfaces || reset_plane || update_rect) {

commit 3856c1869232828bf863b1151b0a1f27fb48bf2c

Fix Damage rect regression
The regression introduced in fb2fdd91d3cb99adcb1737131b3f55b69a0bf6ca
cause HVAC button not correctly rendered if click on it quickly

Change-Id: Ibc081e223ac9b86583196c5e5d296bd2c27238f9
Tests: HVAC button correctly rendered
Jira: None
Signed-off-by: Lin Johnson <johnson.lin@intel.com>

common/core/overlaylayer.cpp
@@ -237,6 +237,15 @@ void OverlayLayer::InitializeState(HwcLayer* layer,
   }
 
   surface_damage_ = layer->GetLayerDamage();
+  // In case of layer using blening we need to force partial clear. Otherwise
+  // we see content not getting updated correctly. For example:
+  // on Android enable, settings put system user_rotation 1 and
+  // navigate to settings on Android.
+  if (((blending_ != HWCBlending::kBlendingNone) && !surface_damage_.empty())) {
+    state_ |= kForcePartialClear;
+    surface_damage_ = layer->GetDisplayFrame();
+  }
+
   SetBuffer(layer->GetNativeHandle(), layer->GetAcquireFence(),
             resource_manager, true, layer);
 