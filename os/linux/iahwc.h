7678b1a0a9e7d69cfd3604fca36424b173c4bd1a
Harish Krupo <harish.krupo.kps@intel.com>
2018-04-14 12:59:49 +0530

    Weston: Add api to set layer index
    In the case where the number of layers requested is more than
    the value with which the LayerHashGenerator is initialized,
    the layers get reordered in the map and thus are incorrectly
    rendered.
    To solve this, we let the compositor set a particular layer's index
    or z-order with the following assumption:
    The layer index set by the compositor is numbered from bottom -> top,
    i.e. the bottom most layer has the index of 0 and increases upwards.
    
    Jira: None.
    Test: Weston demos show up correctly.
    
    Signed-off-by: Harish Krupo <harish.krupo.kps@intel.com>

--- os/linux/iahwc.h
+++ os/linux/iahwc.h
@@ -94,6 +94,7 @@ enum iahwc_function_descriptors {
   IAHWC_FUNC_LAYER_SET_DISPLAY_FRAME,
   IAHWC_FUNC_LAYER_SET_SURFACE_DAMAGE,
   IAHWC_FUNC_LAYER_SET_PLANE_ALPHA,
+  IAHWC_FUNC_LAYER_SET_INDEX,
 };
 
 enum iahwc_callback_descriptor {
@@ -199,6 +200,10 @@ typedef int (*IAHWC_PFN_LAYER_SET_PLANE_ALPHA)(iahwc_device_t*,
                                                iahwc_display_t display_handle,
                                                iahwc_layer_t layer_handle,
                                                float alpha);
+typedef int (*IAHWC_PFN_LAYER_SET_INDEX)(iahwc_device_t*,
+                                         iahwc_display_t display_handle,
+                                         iahwc_layer_t layer_handle,
+                                         uint32_t layer_index);
 typedef int (*IAHWC_PFN_VSYNC)(iahwc_callback_data_t data,
                                iahwc_display_t display, int64_t timestamp);
 typedef int (*IAHWC_PFN_PIXEL_UPLOADER)(iahwc_callback_data_t data,
