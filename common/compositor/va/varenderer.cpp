90fd2b3820a8c5d2d9e52880da43fc723ad3bfff
Lin Johnson <johnson.lin@intel.com>
2018-04-18 09:52:05 +0800

    varenderer: layer transform should also consider flip new camera tests add h-flip
    Jira:https://jira01.devtools.intel.com/browse/OAM-61084
    Tests: Camera Orientation of CTS verifier
    Signed-off-by: Lin Johnson <johnson.lin@intel.com>

--- common/compositor/va/varenderer.cpp
+++ common/compositor/va/varenderer.cpp
@@ -292,7 +292,10 @@ bool VARenderer::Draw(const MediaState& state, NativeSurface* surface) {
   param_.filter_flags = filter_flags_;
 // currently rotation is only supported by VA on Android.
 #ifdef DISABLE_CURSOR_PLANE
-  param_.rotation_state = HWCTransformToVA(state.layer_->GetTransform());
+  param_.rotation_state = HWCRotationToVA(state.layer_->GetTransform());
+#if VA_MAJOR_VERSION > 1
+  param_.mirror_state = HWCReflectToVA(state.layer_->GetTransform());
+#endif
 #endif
 
   ScopedVABufferID pipeline_buffer(va_display_);
@@ -487,18 +490,25 @@ bool VARenderer::UpdateCaps() {
   return true;
 }
 
-uint32_t VARenderer::HWCTransformToVA(uint32_t transform) {
-    switch (transform) {
-        case kTransform270:
-            return VA_ROTATION_270;
-        case kTransform180:
-            return VA_ROTATION_180;
-        case kTransform90:
-            return VA_ROTATION_90;
-        default:
-            break;
-    }
-    return VA_ROTATION_NONE;
+uint32_t VARenderer::HWCRotationToVA(uint32_t transform) {
+  if(transform & kTransform270)
+    return VA_ROTATION_90;
+  if(transform & kTransform180)
+    return VA_ROTATION_180;
+  if(transform & kTransform90)
+    return VA_ROTATION_270;
+  return VA_ROTATION_NONE;
 }
+
+#if VA_MAJOR_VERSION > 1
+uint32_t VARenderer::HWCReflectToVA(uint32_t transform) {
+  ahah
+  if(transform & kReflectX)
+    return VA_MIRROR_HORIZONTAL;
+  if(transform & kReflectY)
+   return VA_MIRROR_VERTICAL;
+  return VA_MIRROR_NONE;
+}
+#endif
 
 }  // namespace hwcomposer
