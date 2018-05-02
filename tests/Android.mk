104748d677d6b0fb64a8b49b9952d3a32620b5f2
Alistair Strachan <astrachan@google.com>
2018-05-02 16:55:22 -0700

    drm_hwcomposer: Mark tests as vendor, fix build
    After commit 94bb596, the tests for drm_hwcomposer no longer build,
    because the build system detects that they are linking other vendor
    modules (but installing themselves elsewhere).
    
    This change also adds the libhardware include path explicitly, as the
    implicit include of hardware/libhardware/include is going away.
    
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: Rob Herring <rob.herring@linaro.org>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Sean Paul <seanpaul@google.com>
    Cc: Robert Foss <robert.foss@collabora.com>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Alistair Strachan <astrachan@google.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>

--- tests/Android.mk
+++ tests/Android.mk
@@ -7,6 +7,8 @@ LOCAL_SRC_FILES := \
 	worker_test.cpp
 
 LOCAL_MODULE := hwc-drm-tests
+LOCAL_VENDOR_MODULE := true
+LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_STATIC_LIBRARIES := libdrmhwc_utils
 LOCAL_SHARED_LIBRARIES := hwcomposer.drm
 LOCAL_C_INCLUDES := external/drm_hwcomposer
