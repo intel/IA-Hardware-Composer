bc0911b03a445050f0844928897b15919e3bcc52
Richard Avelar <csravelar@gmail.com>
2018-05-03 17:02:40 -0700

    change

--- travisci/check_patch.py
+++ travisci/check_patch.py
@@ -5,7 +5,6 @@ cmd = "git show origin/master..@ | clang-format-diff-3.9 -p 1 -style=file"
 (diff, err) = subprocess.Popen(cmd, stdout=subprocess.PIPE,
 shell=True).communicate()
 if diff:
-    print("Code formatting is not according to style guidelines. Read
-           https://github.com/intel/IA-Hardware-Composer/wiki/Contributions#coding_style")
+    print("Code formatting is not according to style guidelines. Read https://github.com/intel/IA-Hardware-Composer/wiki/Contributions#coding_style")
     exit(1)
 exit(0)
