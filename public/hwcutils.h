199145fad45047e58776825718dde6e1c9f4a0a6
Kevin Strasser <kevin.strasser@intel.com>
2018-07-03 13:30:16 -0700

    Add doxygen config and document some of the public APIs
    The APIs defined in the public headers represent the boundary between core
    IA-Hardware-Composer and the platform specific implementations. Start adding
    documentation to help specify the expected API behaviors.
    
    Jira: GSE-1575
    Test: doxygen Doxyfile
    Test: firefox html/index.html
    
    Signed-off-by: Kevin Strasser <kevin.strasser@intel.com>

--- public/hwcutils.h
+++ public/hwcutils.h
@@ -22,53 +22,129 @@
 
 namespace hwcomposer {
 
-// Helper functions.
-
-// Call poll() on fd.
-//  - timeout: time in miliseconds to stay blocked before returning if fd
-//  is not ready.
+/**
+ * Wait until a file descriptor has data ready for reading
+ *
+ * @param fd file descriptor for an open file
+ * @param timeout number of milliseconds to block while waiting for the file
+ * descriptor to become ready.
+ * @return 1 on success
+ * @return 0 on timeout
+ * @return -1 on error
+ */
 int HWCPoll(int fd, int timeout);
 
-// Reset's rect to include region hwc_region.
+/**
+ * Reset the bounds of a rectangle to enclose all rectangles in a region
+ *
+ * If the region is empty then the bounds of the rectangle will be set to 0.
+ * Previous bounds of the rectangle will be reset.
+ * @param hwc_region The region to enclose
+ * @param rect The rectangle to reset
+ */
 void ResetRectToRegion(const HwcRegion& hwc_region, HwcRect<int>& rect);
 
-// Calculate new_rect to include target_rect. This is used to calculate
-// Display Frame and Surface Damage rect of layers.
+/**
+ * Expand the bounds of a rectangle to enclose the bounds of a target rectangle
+ *
+ * Has no effect if the target rectangle has no bounds.
+ * Has no effect if the target rectangle is already enclosed.
+ * @param target_rect The rectangle to enclose
+ * @param new_rect The rectangle to be expanded
+ */
 void CalculateRect(const HwcRect<int>& target_rect, HwcRect<int>& new_rect);
 
-// Calculate new_rect to include target_rect. This is used to calculate
-// Source Rect of layers.
+/**
+ * Expand the bounds of a rectangle to enclose the bounds of a target rectangle
+ *
+ * Has no effect if the target rectangle has no bounds.
+ * Has no effect if the target rectangle is already enclosed.
+ * @param target_rect The rectangle to enclose
+ * @param new_rect The rectangle to be expanded
+ */
 void CalculateSourceRect(const HwcRect<float>& target_rect,
                          HwcRect<float>& new_rect);
 
-// Returns true if format is a Media format.
+/**
+ * Check if a format is used for media
+ *
+ * @param format fourcc based pixel format (see drm_fourcc.h)
+ * @return True for recognized media formats
+ */
 bool IsSupportedMediaFormat(uint32_t format);
 
-// Returns total planes for a given format.
+/**
+ * Check how many planes are used for a given pixel format
+ *
+ * @param format fourcc based pixel format (see drm_fourcc.h)
+ * @return The number of planes for a given format
+ */
 uint32_t GetTotalPlanesForFormat(uint32_t format);
 
+/**
+ * Check if two rectangles overlap
+ *
+ * Top-left is inclusive; bottom-right is exclusive.
+ * Origin is top-left of the coordinate plane.
+ * @param l1 left bound of rectangle 1
+ * @param t1 top bound of rectangle 1
+ * @param r1 right bound of rectangle 1
+ * @param b1 bottom bound of rectangle 1
+ * @param l2 left bound of rectangle 2
+ * @param t2 top bound of rectangle 2
+ * @param r2 right bound of rectangle 2
+ * @param b2 bottom bound of rectangle 2
+ * @return True if rectangles overlap
+ */
 template <class T>
 inline bool IsOverlapping(T l1, T t1, T r1, T b1, T l2, T t2, T r2, T b2)
-// Do two rectangles overlap?
-// Top-left is inclusive; bottom-right is exclusive
+
 {
   return ((l1 < r2 && r1 > l2) && (t1 < b2 && b1 > t2));
 }
 
+/**
+ * Check if two rectangles overlap
+ *
+ * Top-left is inclusive; bottom-right is exclusive.
+ * @param rect1 rectangle 1
+ * @param rect2 rectangle 2
+ * @return True if rectangles overlap
+ */
 inline bool IsOverlapping(const hwcomposer::HwcRect<int>& rect1,
                           const hwcomposer::HwcRect<int>& rect2) {
   return IsOverlapping(rect1.left, rect1.top, rect1.right, rect1.bottom,
                        rect2.left, rect2.top, rect2.right, rect2.bottom);
 }
 
+/**
+ * Check if one rectangle is enclosed by another
+ *
+ * Top-left is inclusive; bottom-right is exclusive.
+ * Origin is top-left of the coordinate plane.
+ * @param l1 left bound of rectangle 1
+ * @param t1 top bound of rectangle 1
+ * @param r1 right bound of rectangle 1
+ * @param b1 bottom bound of rectangle 1
+ * @param l2 left bound of rectangle 2
+ * @param t2 top bound of rectangle 2
+ * @param r2 right bound of rectangle 2
+ * @param b2 bottom bound of rectangle 2
+ * @return True if rectangle 1 is enclosed by rectangle 2
+ */
 template <class T>
-inline bool IsEnclosedBy(T l1, T t1, T r1, T b1, T l2, T t2, T r2, T b2)
-// Do two rectangles overlap?
-// Top-left is inclusive; bottom-right is exclusive
-{
+inline bool IsEnclosedBy(T l1, T t1, T r1, T b1, T l2, T t2, T r2, T b2) {
   return ((l1 >= l2 && t1 >= t2) && (r1 <= r2 && b1 <= b2));
 }
 
+/**
+ * Check if one rectangle is enclosed by another
+ *
+ * Top-left is inclusive; bottom-right is exclusive.
+ * @param rect1 rectangle 1
+ * @param rect2 rectangle 2
+ * @return True if rectangle 1 is enclosed by rectangle 2
+ */
 inline bool IsEnclosedBy(const hwcomposer::HwcRect<int>& rect1,
                          const hwcomposer::HwcRect<int>& rect2) {
   return IsEnclosedBy(rect1.left, rect1.top, rect1.right, rect1.bottom,
@@ -77,6 +153,17 @@ inline bool IsEnclosedBy(const hwcomposer::HwcRect<int>& rect1,
 
 enum OverlapType { kEnclosed = 0, kOverlapping, kOutside };
 
+/**
+ * Check the bounds overlap of two rectangles
+ *
+ * Top-left is inclusive; bottom-right is exclusive.
+ * @param rect rectangle 1
+ * @param bounds rectangle 2
+ * @return kEnclosed if rectangle 1 is enclosed by rectangle 2
+ * @return kOverlapping if rectangle 1 overlaps with but is not enclosed by
+ * rectangle 2
+ * @return kOutside if the two rectangles are separate
+ */
 inline OverlapType AnalyseOverlap(const hwcomposer::HwcRect<int>& rect,
                                   const hwcomposer::HwcRect<int>& bounds) {
   if (IsEnclosedBy(rect, bounds)) {
@@ -88,6 +175,14 @@ inline OverlapType AnalyseOverlap(const hwcomposer::HwcRect<int>& rect,
   }
 }
 
+/**
+ * Translate a rectangle across the coordinate plane
+ *
+ * Origin is top-left of the coordinate plane
+ * @param x number of units to translate rightward
+ * @param y number of units to translate downward
+ * @return Translated rectangle
+ */
 inline HwcRect<int> TranslateRect(HwcRect<int> rect, int x, int y) {
   HwcRect<int> ret;
   ret.left = rect.left + x;
@@ -97,6 +192,14 @@ inline HwcRect<int> TranslateRect(HwcRect<int> rect, int x, int y) {
   return ret;
 }
 
+/**
+ * Find the rectangle that bounds the intersection between two rectangles
+ *
+ * @param rect1 rectangle 1
+ * @param rect2 rectangle 2
+ * @return Rectangle bounding the intersection
+ * @return Empty rectangle if no intersection is found
+ */
 inline HwcRect<int> Intersection(const hwcomposer::HwcRect<int>& rect1,
                                  const hwcomposer::HwcRect<int>& rect2) {
   HwcRect<int> rect = {0, 0, 0, 0};
