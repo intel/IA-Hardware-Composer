0e9eb9889690d51f9758f72be285687e57c09228
Richard Avelar <richard.avelar@intel.com>
2018-08-01 10:30:43 -0700

    Add documentation to logicaldisplaymanager.h
    This patch adds documentation for doxygen for the file
    logicaldisplaymanager.h
    
    Jira: GSE-1575
    Tests: Run Doxygen
    Signed-off-by: Richard Avelar richard.avelar@intel.com

--- common/core/logicaldisplaymanager.h
+++ common/core/logicaldisplaymanager.h
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 */
-
+/** \file */
 #ifndef WSI_LOGICALDISPLAY_MANAGER_H_
 #define WSI_LOGICALDISPLAY_MANAGER_H_
 
@@ -29,29 +29,72 @@ class LogicalDisplayManager;
 
 class LogicalDisplayManager {
  public:
+  /**
+   * Constructor that sets the physical display for the class.
+   *
+   * @param physical_display NativeDisplay object for a physical display
+   */
   LogicalDisplayManager(NativeDisplay* physical_display);
   ~LogicalDisplayManager();
 
+  /**
+   * Initialize new logical displays up to the passed in total count.
+   *
+   * @param total the amount of logical displays to initialize
+   */
   void InitializeLogicalDisplays(uint32_t total);
+  /**
+   * Switch the physical display's power mode to on or off from previous state.
+   */
   void UpdatePowerMode();
+  /**
+   * Enable or disable the VSyncControl for physical display's EnableVSync.
+   */
   void UpdateVSyncControl();
+  /**
+   * Register hot plug and notifications if hot plug not already registered.
+   */
   void RegisterHotPlugNotification();
 
   bool Present(std::vector<HwcLayer*>& source_layers, int32_t* retire_fence,
                PixelUploaderCallback* call_back, bool handle_constraints);
 
+  /**
+   * Run all display's VSyncUpdate and pass timestamp to each call.
+   *
+   * @param timestamp a 64 bit integer representation of a Unix timestamp.
+   */
   void VSyncCallback(int64_t timestamp);
 
+  /**
+   * Run all display's RefreshUpdate
+   */
   void RefreshCallback();
 
+  /**
+   * Run all display's HotPlugUpdate and pass the parameter connected to each.
+   *
+   * @param connected a flag indicating if physical display is connected or not
+   */
   void HotPlugCallback(bool connected);
 
   NativeDisplay* GetPhysicalDisplay() const {
     return physical_display_;
   }
 
+  /**
+   * Get each display and append them into the parameter displays array.
+   *
+   * @param displays array of LogicalDisplays to store retrieved displays
+   */
   void GetLogicalDisplays(std::vector<LogicalDisplay*>& displays);
 
+  /**
+   * Get the state and content_type pass it to set all displays' SetHDCPState.
+   *
+   * @param state an enum named HWCContentProtection
+   * @param content_type an enum of HWCContentType
+   */
   void SetHDCPState(HWCContentProtection state, HWCContentType content_type);
 
  private:
