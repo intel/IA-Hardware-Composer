intel/IA-Hardware-Composer

commit b6ff1590f362fefadbc0ad3b3128fef9895e28be

FrameBufferManager Instance owned by GPUDevice
This patch addresses issue #303 and has the GpuDevice take ownership
of the FrameBufferManager Instance which then gets passed to all
displays on initialization to pass to the drmbuffer and the
compositorthread as needed in place of calling the instances from
FrameBufferManager directly.

Jira: None
Test: HWC build passes without errors
Signed-off-by: Richard Avelar richard.avelar@intel.com

common/compositor/compositor.cpp
@@ -40,11 +40,12 @@ Compositor::Compositor() {
 Compositor::~Compositor() {
 }
 
-void Compositor::Init(ResourceManager *resource_manager, uint32_t gpu_fd) {
+void Compositor::Init(ResourceManager *resource_manager, uint32_t gpu_fd,
+                      FrameBufferManager *frame_buffer_manager) {
   if (!thread_)
     thread_.reset(new CompositorThread());
 
-  thread_->Initialize(resource_manager, gpu_fd);
+  thread_->Initialize(resource_manager, gpu_fd, frame_buffer_manager);
 }
 
 void Compositor::BeginFrame(bool disable_explicit_sync) {

common/compositor/compositor.h
@@ -41,7 +41,8 @@ class Compositor {
   Compositor &operator=(const Compositor &) = delete;
   ~Compositor();
 
-  void Init(ResourceManager *buffer_manager, uint32_t gpu_fd);
+  void Init(ResourceManager *buffer_manager, uint32_t gpu_fd,
+            FrameBufferManager *frame_buffer_manager);
   void Reset();
   void BeginFrame(bool disable_explicit_sync);
   bool Draw(DisplayPlaneStateList &planes, std::vector<OverlayLayer> &layers,

common/compositor/nativesurface.cpp
@@ -44,7 +44,9 @@ NativeSurface::~NativeSurface() {
 
 bool NativeSurface::Init(ResourceManager *resource_manager, uint32_t format,
                          uint32_t usage, uint64_t modifier,
-                         bool *modifier_succeeded) {
+                         bool *modifier_succeeded,
+                         FrameBufferManager *frame_buffer_manager) {
+  fb_manager_ = frame_buffer_manager;
   const NativeBufferHandler *handler =
       resource_manager->GetNativeBufferHandler();
   resource_manager_ = resource_manager;
@@ -210,7 +212,7 @@ void NativeSurface::ResetDamage() {
 
 void NativeSurface::InitializeLayer(HWCNativeHandle native_handle) {
   layer_.SetBlending(HWCBlending::kBlendingPremult);
-  layer_.SetBuffer(native_handle, -1, resource_manager_, false);
+  layer_.SetBuffer(native_handle, -1, resource_manager_, false, fb_manager_);
 }
 
 }  // namespace hwcomposer

common/compositor/nativesurface.h
@@ -24,6 +24,7 @@
 
 namespace hwcomposer {
 
+class FrameBufferManager;
 class ResourceManager;
 class DisplayPlaneState;
 
@@ -43,7 +44,8 @@ class NativeSurface {
   virtual ~NativeSurface();
 
   bool Init(ResourceManager* resource_manager, uint32_t format, uint32_t usage,
-            uint64_t modifier, bool* modifier_succeeded);
+            uint64_t modifier, bool* modifier_succeeded,
+            FrameBufferManager* frame_buffer_manager);
 
   bool InitializeForOffScreenRendering(HWCNativeHandle native_handle,
                                        ResourceManager* resource_manager);
@@ -150,6 +152,7 @@ class NativeSurface {
   bool on_screen_ = false;
   HwcRect<int> previous_damage_;
   HwcRect<int> previous_nc_damage_;
+  FrameBufferManager* fb_manager_ = NULL;
 };
 
 }  // namespace hwcomposer

common/core/nesteddisplay.cpp
@@ -158,7 +158,8 @@ void NestedDisplay::InitNestedDisplay(uint32_t width, uint32_t height,
 #endif
 }
 
-bool NestedDisplay::Initialize(NativeBufferHandler * /*buffer_handler*/) {
+bool NestedDisplay::Initialize(NativeBufferHandler * /*buffer_handler*/,
+                               FrameBufferManager * /*frame_buffer_manager*/) {
   return true;
 }
 

common/core/nesteddisplay.h
@@ -102,7 +102,8 @@ class NestedDisplay : public NativeDisplay {
   void InitNestedDisplay(uint32_t width, uint32_t height,
                          uint32_t port) override;
 
-  bool Initialize(NativeBufferHandler *buffer_handler) override;
+  bool Initialize(NativeBufferHandler *buffer_handler,
+                  FrameBufferManager * /*frame_buffer_manager*/) override;
 
   DisplayType Type() const override {
     return DisplayType::kNested;

common/display/virtualdisplay.cpp
@@ -40,7 +40,7 @@ VirtualDisplay::VirtualDisplay(uint32_t gpu_fd,
     ETRACE("Failed to construct hwc layer buffer manager");
   }
 
-  compositor_.Init(resource_manager_.get(), gpu_fd);
+  compositor_.Init(resource_manager_.get(), gpu_fd, fb_manager_);
 }
 
 VirtualDisplay::~VirtualDisplay() {
@@ -110,7 +110,7 @@ bool VirtualDisplay::Present(std::vector<HwcLayer *> &source_layers,
 
     overlay_layer.InitializeFromHwcLayer(
         layer, resource_manager_.get(), previous_layer, z_order, layer_index,
-        height_, kIdentity, handle_constraints);
+        height_, kIdentity, handle_constraints, fb_manager_);
     index.emplace_back(z_order);
     layers_rects.emplace_back(layer->GetDisplayFrame());
     z_order++;
@@ -196,7 +196,9 @@ void VirtualDisplay::SetOutputBuffer(HWCNativeHandle buffer,
   }
 }
 
-bool VirtualDisplay::Initialize(NativeBufferHandler * /*buffer_manager*/) {
+bool VirtualDisplay::Initialize(NativeBufferHandler * /*buffer_manager*/,
+                                FrameBufferManager *frame_buffer_manager) {
+  fb_manager_ = frame_buffer_manager;
   return true;
 }
 

common/display/virtualdisplay.h
@@ -27,6 +27,7 @@
 
 namespace hwcomposer {
 struct HwcLayer;
+class FrameBufferManager;
 class NativeBufferHandler;
 
 class VirtualDisplay : public NativeDisplay {
@@ -49,7 +50,8 @@ class VirtualDisplay : public NativeDisplay {
 
   void SetOutputBuffer(HWCNativeHandle buffer, int32_t acquire_fence) override;
 
-  bool Initialize(NativeBufferHandler *buffer_handler) override;
+  bool Initialize(NativeBufferHandler *buffer_handler,
+                  FrameBufferManager *frame_buffer_manager) override;
 
   DisplayType Type() const override {
     return DisplayType::kVirtual;
@@ -91,6 +93,7 @@ class VirtualDisplay : public NativeDisplay {
   std::vector<OverlayLayer> in_flight_layers_;
   HWCNativeHandle handle_ = 0;
   std::unique_ptr<ResourceManager> resource_manager_;
+  FrameBufferManager *fb_manager_ = NULL;
 };
 
 }  // namespace hwcomposer

commit 79475a8d4cd33617bd25521b5bbc807a0a38e68b

Fix virtual display regression caused by b6ff1590f362fefadbc0ad3b3128fef9895e28be virtual display and nested display should have fb_manager in constructor because compositor is intialized in constructor
Jira: https://jira01.devtools.intel.com/browse/OAM-67091
Tests: virtual display on Android P GP2.0
Signed-off-by: Lin Johnson <johnson.lin@intel.com>

common/compositor/compositor.cpp
@@ -152,6 +152,7 @@ bool Compositor::DrawOffscreen(std::vector<OverlayLayer> &layers,
                                const std::vector<HwcRect<int>> &display_frame,
                                const std::vector<size_t> &source_layers,
                                ResourceManager *resource_manager,
+                               FrameBufferManager *framebuffer_manager,
                                uint32_t width, uint32_t height,
                                HWCNativeHandle output_handle,
                                int32_t acquire_fence, int32_t *retire_fence) {
@@ -167,7 +168,8 @@ bool Compositor::DrawOffscreen(std::vector<OverlayLayer> &layers,
   }
 
   NativeSurface *surface = Create3DBuffer(width, height);
-  surface->InitializeForOffScreenRendering(output_handle, resource_manager);
+  surface->InitializeForOffScreenRendering(output_handle, resource_manager,
+                                           framebuffer_manager);
   std::vector<DrawState> draw;
   std::vector<DrawState> media;
   draw.emplace_back();

common/compositor/compositor.h
@@ -50,7 +50,8 @@ class Compositor {
   bool DrawOffscreen(std::vector<OverlayLayer> &layers,
                      const std::vector<HwcRect<int>> &display_frame,
                      const std::vector<size_t> &source_layers,
-                     ResourceManager *resource_manager, uint32_t width,
+                     ResourceManager *resource_manager,
+                     FrameBufferManager *framebuffer_manager, uint32_t width,
                      uint32_t height, HWCNativeHandle output_handle,
                      int32_t acquire_fence, int32_t *retire_fence);
   void FreeResources();

common/compositor/nativesurface.cpp
@@ -94,8 +94,10 @@ bool NativeSurface::Init(ResourceManager *resource_manager, uint32_t format,
 }
 
 bool NativeSurface::InitializeForOffScreenRendering(
-    HWCNativeHandle native_handle, ResourceManager *resource_manager) {
+    HWCNativeHandle native_handle, ResourceManager *resource_manager,
+    FrameBufferManager *framebuffer_manager) {
   resource_manager_ = resource_manager;
+  fb_manager_ = framebuffer_manager;
   InitializeLayer(native_handle);
   layer_.SetSourceCrop(HwcRect<float>(0, 0, width_, height_));
   layer_.SetDisplayFrame(HwcRect<int>(0, 0, width_, height_));

common/compositor/nativesurface.h
@@ -47,8 +47,9 @@ class NativeSurface {
             uint64_t modifier, bool* modifier_succeeded,
             FrameBufferManager* frame_buffer_manager);
 
-  bool InitializeForOffScreenRendering(HWCNativeHandle native_handle,
-                                       ResourceManager* resource_manager);
+  bool InitializeForOffScreenRendering(
+      HWCNativeHandle native_handle, ResourceManager* resource_manager,
+      FrameBufferManager* frame_buffer_manager);
 
   virtual bool MakeCurrent() {
     return false;

common/core/nesteddisplay.cpp
@@ -70,13 +70,15 @@ void SocketThread::HandleRoutine() {
 #endif
 
 NestedDisplay::NestedDisplay(uint32_t gpu_fd,
-                             NativeBufferHandler *buffer_handler) {
+                             NativeBufferHandler *buffer_handler,
+                             FrameBufferManager *framebuffermanager) {
 #ifdef NESTED_DISPLAY_SUPPORT
   int ret;
   struct ioctl_hyper_dmabuf_tx_ch_setup msg;
   memset(&msg, 0, sizeof(ioctl_hyper_dmabuf_tx_ch_setup));
 
   resource_manager_.reset(new ResourceManager(buffer_handler));
+  fb_manger_ = framebuffermanager;
   if (!resource_manager_) {
     ETRACE("Failed to construct hwc layer buffer manager");
   }

common/core/nesteddisplay.h
@@ -97,7 +97,8 @@ class SocketThread : public HWCThread {
 
 class NestedDisplay : public NativeDisplay {
  public:
-  NestedDisplay(uint32_t gpu_fd, NativeBufferHandler *buffer_handler);
+  NestedDisplay(uint32_t gpu_fd, NativeBufferHandler *buffer_handler,
+                FrameBufferManager *framebuffermanager);
   ~NestedDisplay() override;
 
   void InitNestedDisplay(uint32_t width, uint32_t height,

common/display/virtualdisplay.cpp
@@ -33,13 +33,14 @@ namespace hwcomposer {
 
 VirtualDisplay::VirtualDisplay(uint32_t gpu_fd,
                                NativeBufferHandler *buffer_handler,
+                               FrameBufferManager *frame_buffer_manager,
                                uint32_t /*pipe_id*/, uint32_t /*crtc_id*/)
     : output_handle_(0), acquire_fence_(-1), width_(0), height_(0) {
   resource_manager_.reset(new ResourceManager(buffer_handler));
   if (!resource_manager_) {
     ETRACE("Failed to construct hwc layer buffer manager");
   }
-
+  fb_manager_ = frame_buffer_manager;
   compositor_.Init(resource_manager_.get(), gpu_fd, fb_manager_);
 }
 
@@ -133,8 +134,8 @@ bool VirtualDisplay::Present(std::vector<HwcLayer *> &source_layers,
 
     // Prepare for final composition.
     if (!compositor_.DrawOffscreen(
-            layers, layers_rects, index, resource_manager_.get(), width_,
-            height_, output_handle_, acquire_fence_, retire_fence)) {
+            layers, layers_rects, index, resource_manager_.get(), fb_manager_,
+            width_, height_, output_handle_, acquire_fence_, retire_fence)) {
       ETRACE("Failed to prepare for the frame composition ret=%d", ret);
       return false;
     }
@@ -198,7 +199,6 @@ void VirtualDisplay::SetOutputBuffer(HWCNativeHandle buffer,
 
 bool VirtualDisplay::Initialize(NativeBufferHandler * /*buffer_manager*/,
                                 FrameBufferManager *frame_buffer_manager) {
-  fb_manager_ = frame_buffer_manager;
   return true;
 }
 

common/display/virtualdisplay.h
@@ -33,7 +33,8 @@ class NativeBufferHandler;
 class VirtualDisplay : public NativeDisplay {
  public:
   VirtualDisplay(uint32_t gpu_fd, NativeBufferHandler *buffer_handler,
-                 uint32_t pipe_id, uint32_t crtc_id);
+                 FrameBufferManager *framebuffermanager, uint32_t pipe_id,
+                 uint32_t crtc_id);
   VirtualDisplay(const VirtualDisplay &) = delete;
   VirtualDisplay &operator=(const VirtualDisplay &) = delete;
   ~VirtualDisplay() override;
@@ -94,6 +95,7 @@ class VirtualDisplay : public NativeDisplay {
   HWCNativeHandle handle_ = 0;
   std::unique_ptr<ResourceManager> resource_manager_;
   FrameBufferManager *fb_manager_ = NULL;
+  uint32_t gpu_fd_ = 0;
 };
 
 }  // namespace hwcomposer

wsi/drm/drmdisplaymanager.cpp
@@ -182,8 +182,10 @@ void DrmDisplayManager::InitializeDisplayResources() {
     }
   }
 
-  virtual_display_.reset(new VirtualDisplay(fd_, buffer_handler_.get(), 0, 0));
-  nested_display_.reset(new NestedDisplay(fd_, buffer_handler_.get()));
+  virtual_display_.reset(new VirtualDisplay(fd_, buffer_handler_.get(),
+                                            frame_buffer_manager_.get(), 0, 0));
+  nested_display_.reset(new NestedDisplay(fd_, buffer_handler_.get(),
+                                          frame_buffer_manager_.get()));
 }
 
 void DrmDisplayManager::StartHotPlugMonitor() {