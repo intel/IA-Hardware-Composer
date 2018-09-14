intel/IA-Hardware-Composer

commit 21c2f60dfd5044cfa0e640b09d9c42e52a669f00

Should make sure separate plane for video when required media effects, since VPP path only applies to a separate plane. Add a variable in display queue to track if media effect changes If it changed, should not use idle frame
Jira: None
Tests: video playback with media effect changes

common/display/displayqueue.cpp
@@ -696,6 +696,11 @@ bool DisplayQueue::QueueUpdate(std::vector<HwcLayer*>& source_layers,
     }
   }
 
+  if (has_video_layer && video_effect_changed_) {
+    video_effect_changed_ = false;
+    idle_frame = false;
+  }
+
   if (!validate_layers)
     validate_layers = idle_frame;
 #ifdef SURFACE_TRACING
@@ -723,10 +728,9 @@ bool DisplayQueue::QueueUpdate(std::vector<HwcLayer*>& source_layers,
   bool requested_video_effect = false;
   if (has_video_layer) {
     video_lock_.lock();
-    if (requested_video_effect_ != applied_video_effect_) {
+    if (requested_video_effect_) {
       // Let's ensure Media planes take this into account.
       force_media_composition = true;
-      applied_video_effect_ = requested_video_effect_;
       requested_video_effect = requested_video_effect_;
       idle_frame = false;
       validate_layers = true;
@@ -1440,7 +1444,8 @@ void DisplayQueue::SetExplicitSyncSupport(bool disable_explicit_sync) {
 
 void DisplayQueue::SetVideoScalingMode(uint32_t mode) {
   video_lock_.lock();
-  // requested_video_effect_ = true;
+  requested_video_effect_ = true;
+  video_effect_changed_ = true;
   compositor_.SetVideoScalingMode(mode);
   video_lock_.unlock();
 }
@@ -1448,6 +1453,7 @@ void DisplayQueue::SetVideoScalingMode(uint32_t mode) {
 void DisplayQueue::SetVideoColor(HWCColorControl color, float value) {
   video_lock_.lock();
   requested_video_effect_ = true;
+  video_effect_changed_ = true;
   compositor_.SetVideoColor(color, value);
   video_lock_.unlock();
 }
@@ -1460,6 +1466,7 @@ void DisplayQueue::GetVideoColor(HWCColorControl color, float* value,
 void DisplayQueue::RestoreVideoDefaultColor(HWCColorControl color) {
   video_lock_.lock();
   requested_video_effect_ = false;
+  video_effect_changed_ = true;
   compositor_.RestoreVideoDefaultColor(color);
   video_lock_.unlock();
 }
@@ -1468,6 +1475,7 @@ void DisplayQueue::SetVideoDeinterlace(HWCDeinterlaceFlag flag,
                                        HWCDeinterlaceControl mode) {
   video_lock_.lock();
   requested_video_effect_ = true;
+  video_effect_changed_ = true;
   compositor_.SetVideoDeinterlace(flag, mode);
   video_lock_.unlock();
 }
@@ -1475,6 +1483,7 @@ void DisplayQueue::SetVideoDeinterlace(HWCDeinterlaceFlag flag,
 void DisplayQueue::RestoreVideoDefaultDeinterlace() {
   video_lock_.lock();
   requested_video_effect_ = false;
+  video_effect_changed_ = true;
   compositor_.RestoreVideoDefaultDeinterlace();
   video_lock_.unlock();
 }
@@ -1579,7 +1588,6 @@ void DisplayQueue::UpdateScalingRatio(uint32_t primary_width,
 }
 
 void DisplayQueue::ResetQueue() {
-  applied_video_effect_ = false;
   last_commit_failed_update_ = false;
   std::vector<OverlayLayer>().swap(in_flight_layers_);
   DisplayPlaneStateList().swap(previous_plane_state_);

common/display/displayqueue.h
@@ -365,7 +365,7 @@ class DisplayQueue {
   uint32_t plane_transform_ = kIdentity;
   SpinLock video_lock_;
   bool requested_video_effect_ = false;
-  bool applied_video_effect_ = false;
+  bool video_effect_changed_ = false;
   // Set to true when layers are validated and commit fails.
   bool last_commit_failed_update_ = false;
   // Set to true if cloned display needs to be validated.

commit 19c940d1943c773a5d1ebe8eb0a935299680c678

Removing deinterlace frame by frame check as it cause FPS regression for video Also fixed performance regression caused by 21c2f60dfd5044cfa0e640b09d9c42e52a669f00 validate_Layers shoud apply only when video effect changes
Jira://jira01.devtools.intel.com/browse/OAM-68648
Tests: Video FPS no regression
Signed-off-by: Lin Johnson <johnson.lin@intel.com>

common/compositor/va/varenderer.cpp
@@ -223,7 +223,7 @@ unsigned int VARenderer::GetVAProcFilterScalingMode(uint32_t mode) {
       case 2:
         return VA_FILTER_SCALING_HQ;
       default:
-        return VA_FILTER_SCALING_FAST;
+        return VA_FILTER_SCALING_HQ;
     }
   } else
     return VA_FILTER_SCALING_FAST;

common/display/displayqueue.cpp
@@ -696,11 +696,6 @@ bool DisplayQueue::QueueUpdate(std::vector<HwcLayer*>& source_layers,
     }
   }
 
-  if (has_video_layer && video_effect_changed_) {
-    video_effect_changed_ = false;
-    idle_frame = false;
-  }
-
   if (!validate_layers)
     validate_layers = idle_frame;
 #ifdef SURFACE_TRACING
@@ -728,12 +723,16 @@ bool DisplayQueue::QueueUpdate(std::vector<HwcLayer*>& source_layers,
   bool requested_video_effect = false;
   if (has_video_layer) {
     video_lock_.lock();
+    if (video_effect_changed_) {
+      idle_frame = false;
+      video_effect_changed_ = false;
+      validate_layers = true;
+    }
     if (requested_video_effect_) {
       // Let's ensure Media planes take this into account.
       force_media_composition = true;
       requested_video_effect = requested_video_effect_;
       idle_frame = false;
-      validate_layers = true;
     }
     video_lock_.unlock();
   }

os/android/gralloc1bufferhandler.cpp
@@ -60,8 +60,7 @@ Gralloc1BufferHandler::Gralloc1BufferHandler(uint32_t fd)
       set_format_(nullptr),
       set_producer_usage_(nullptr),
       allocate_(nullptr),
-      set_modifier_(nullptr),
-      get_interlace_(nullptr) {
+      set_modifier_(nullptr) {
 }
 
 Gralloc1BufferHandler::~Gralloc1BufferHandler() {
@@ -123,9 +122,6 @@ bool Gralloc1BufferHandler::Init() {
   set_modifier_ = reinterpret_cast<GRALLOC1_PFN_SET_MODIFIER>(
       gralloc1_dvc->getFunction(gralloc1_dvc, GRALLOC1_FUNCTION_SET_MODIFIER));
 #endif
-
-  get_interlace_ = reinterpret_cast<GRALLOC1_PFN_GET_INTERLACE>(
-      gralloc1_dvc->getFunction(gralloc1_dvc, GRALLOC1_FUNCTION_GET_INTERLACE));
   return true;
 }
 
@@ -249,15 +245,6 @@ bool Gralloc1BufferHandler::ImportBuffer(HWCNativeHandle handle) const {
     return false;
   }
 
-  if (handle->meta_data_.usage_ == hwcomposer::kLayerVideo) {
-    uint32_t interlace = 0;
-    get_interlace_(gralloc1_dvc, handle->imported_handle_, &interlace);
-    if (interlace > 0) {
-      handle->meta_data_.is_interlaced_ = true;
-    } else
-      handle->meta_data_.is_interlaced_ = false;
-  }
-
   return true;
 }
 
@@ -311,13 +298,10 @@ int32_t Gralloc1BufferHandler::UnMap(HWCNativeHandle handle,
 }
 
 bool Gralloc1BufferHandler::GetInterlace(HWCNativeHandle handle) const {
-  uint32_t interlace = 0;
-  gralloc1_device_t *gralloc1_dvc =
-      reinterpret_cast<gralloc1_device_t *>(device_);
-  get_interlace_(gralloc1_dvc, handle->imported_handle_, &interlace);
-  if (interlace > 0)
+  if (((struct cros_gralloc_handle *)handle->handle_)->is_interlaced > 0)
     return true;
-  return false;
+  else
+    return false;
 }
 
 }  // namespace hwcomposer

os/android/gralloc1bufferhandler.h
@@ -72,7 +72,6 @@ class Gralloc1BufferHandler : public NativeBufferHandler {
   GRALLOC1_PFN_SET_FORMAT set_format_;
   GRALLOC1_PFN_SET_PRODUCER_USAGE set_producer_usage_;
   GRALLOC1_PFN_ALLOCATE allocate_;
-  GRALLOC1_PFN_GET_INTERLACE get_interlace_;
 #ifdef USE_GRALLOC1
   GRALLOC1_PFN_SET_MODIFIER set_modifier_;
 #endif

os/android/utils_android.h
@@ -218,6 +218,11 @@ static bool ImportGraphicsBuffer(HWCNativeHandle handle, int fd) {
   handle->meta_data_.width_ = gr_handle->width;
   handle->meta_data_.height_ = gr_handle->height;
   handle->meta_data_.native_format_ = gr_handle->droid_format;
+  if (gr_handle->is_interlaced > 0) {
+    handle->meta_data_.is_interlaced_ = true;
+  } else {
+    handle->meta_data_.is_interlaced_ = false;
+  }
 
   int32_t numplanes = gr_handle->base.numFds;
   handle->meta_data_.num_planes_ = numplanes;