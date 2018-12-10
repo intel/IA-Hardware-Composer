c2a6fb8a9783ce54eb7310f75e846832c4365f8a
Wan Shuang <shuang.wan@intel.com>
2018-12-10 18:28:37 +0800

    Fixed the HWC crash caused by map iterator erase.
    Erase map iterator during iterating a map caused the crash.
    The fix is to avoid erase itator in the interating loop and clear
    the iterator after the loop end.
    
    Change-Id: I409a6293ad290b7aaae8f2f4ea6e971cc29cf34a
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-71721
    Tests: Trigger VirtualDisplay destructor condition and no HWC crash.
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>

--- common/display/virtualdisplay.cpp
+++ common/display/virtualdisplay.cpp
@@ -88,7 +88,10 @@ VirtualDisplay::~VirtualDisplay() {
     resource_manager_->MarkResourceForDeletion(temp, false);
   }
 
-  delete output_handle_;
+  if (output_handle_) {
+    delete output_handle_;
+  }
+
   std::vector<OverlayLayer>().swap(in_flight_layers_);
 
   resource_manager_->PurgeBuffer();
@@ -111,9 +114,12 @@ VirtualDisplay::~VirtualDisplay() {
       } else {
         ITRACE("Hyper DmaBuf: IOCTL_HYPER_DMABUF_UNEXPORT ioctl Done [0x%x]!\n",
                it->second.hyper_dmabuf_id.id);
-        mHyperDmaExportedBuffers.erase(it);
       }
     }
+    /* Clear up the map of exported buffers whatever if the ioctl of
+     * IOCTL_HYPER_DMABUF_UNEXPORTED success or fail.
+     */
+    mHyperDmaExportedBuffers.clear();
 
     close(mHyperDmaBuf_Fd);
     mHyperDmaBuf_Fd = -1;
