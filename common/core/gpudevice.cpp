intel/IA-Hardware-Composer

commit 37f472c449f900145bbc920ee842edc1a1b3023e

Shorten the HandleHWCSettings function
Split the function to sub-function.

Change-Id: I1d32836636124dd45c31a0bdd17413ee39448a31
Tracked-On: https://jira.devtools.intel.com/browse/OAM-76800
Tests: Compile sucessful for Android, all display mode works well.
Signed-off-by: HeYue <yue.he@intel.com>

public/gpudevice.h
@@ -123,12 +123,47 @@ class GpuDevice : public HWCThread {
   void ParsePanoramaSOSDisplayConfig(
       std::string& value,
       std::vector<std::vector<uint32_t>>& panorama_sos_displays);
-  void PanoramaInit(std::vector<NativeDisplay*>& total_displays_,
-                    std::vector<NativeDisplay*>& temp_displays,
-                    std::vector<std::vector<uint32_t>>& panorama_displays,
-                    std::vector<std::vector<uint32_t>>& panorama_sos_displays,
-                    std::vector<bool>& available_displays);
+  void InitializePanorama(
+      std::vector<NativeDisplay*>& total_displays_,
+      std::vector<NativeDisplay*>& temp_displays,
+      std::vector<std::vector<uint32_t>>& panorama_displays,
+      std::vector<std::vector<uint32_t>>& panorama_sos_displays,
+      std::vector<bool>& available_displays);
 #endif
+  void ParseLogicalDisplaySetting(std::string& value,
+                                  std::vector<uint32_t>& logical_displays);
+  void ParseMosaicDisplaySetting(
+      std::string& value, std::vector<std::vector<uint32_t>>& mosaic_displays);
+  void ParsePhysicalDisplaySetting(std::string& value,
+                                   std::vector<uint32_t>& physical_displays);
+  void ParseCloneDisplaySetting(
+      std::string& value, std::vector<std::vector<uint32_t>>& cloned_displays);
+  void ParsePhysicalDisplayRotation(
+      std::string& value, std::vector<uint32_t>& display_rotation,
+      std::vector<uint32_t>& rotation_display_index);
+  void ParseFloatDisplaySetting(std::string& value,
+                                std::vector<HwcRect<int32_t>>& float_displays,
+                                std::vector<uint32_t>& float_display_indices);
+
+  void InitializeDisplayIndex(std::vector<uint32_t>& physical_displays,
+                              std::vector<NativeDisplay*>& displays);
+  void InitializeLogicalDisplay(std::vector<uint32_t>& logical_displays,
+                                std::vector<NativeDisplay*>& displays,
+                                std::vector<NativeDisplay*>& temp_displays,
+                                bool use_logical);
+  void InitializeDisplayRotation(std::vector<uint32_t>& display_rotation,
+                                 std::vector<uint32_t>& rotation_display_index,
+                                 std::vector<NativeDisplay*>& displays);
+  void InitializeMosaicDisplay(
+      std::vector<NativeDisplay*>& total_displays_,
+      std::vector<std::vector<uint32_t>>& mosaic_displays,
+      std::vector<NativeDisplay*>& temp_displays);
+  void InitializeCloneDisplay(
+      std::vector<NativeDisplay*>& total_displays_,
+      std::vector<std::vector<uint32_t>>& cloned_displays);
+  void InitializeFloatDisplay(std::vector<NativeDisplay*>& total_displays_,
+                              std::vector<HwcRect<int32_t>>& float_displays,
+                              std::vector<uint32_t>& float_display_indices);
   std::vector<NativeDisplay*> total_displays_;
 
   bool reserve_plane_ = false;

commit f8cd31ac60fc3053e5fade24724ace0ff64fc2b2

Fix the ACRN build error
Fix ACRN build error caused by 37f472c449f900145bbc920ee842edc1a1b3023e

Change-Id: I224b4e475ff8a19e3ef989f3080c1b67dbfd433e
Tracked-On: https://jira.devtools.intel.com/browse/OAM-76800
Tests: Compile sucessful for ACRN and baremetal, all display mode works well.
Signed-off-by: HeYue <yue.he@intel.com>

common/core/gpudevice.cpp
@@ -666,8 +666,8 @@ void GpuDevice::InitializeLogicalDisplay(
 void GpuDevice::InitializeMosaicDisplay(
     std::vector<NativeDisplay *> &total_displays_,
     std::vector<std::vector<uint32_t>> &mosaic_displays,
-    std::vector<NativeDisplay *> &temp_displays) {
-  std::vector<bool> available_displays(temp_displays.size(), true);
+    std::vector<NativeDisplay *> &temp_displays,
+    std::vector<bool> &available_displays) {
   size_t displays_size = temp_displays.size();
   for (size_t t = 0; t < displays_size; t++) {
     // Skip the displays which already be marked in other mosaic
@@ -933,8 +933,10 @@ void GpuDevice::HandleHWCSettings() {
   InitializeLogicalDisplay(logical_displays, displays, temp_displays,
                            use_logical);
 
+  std::vector<bool> available_displays(temp_displays.size(), true);
   if (use_mosaic) {
-    InitializeMosaicDisplay(total_displays_, mosaic_displays, temp_displays);
+    InitializeMosaicDisplay(total_displays_, mosaic_displays, temp_displays,
+                            available_displays);
   } else {
     total_displays_.swap(temp_displays);
   }

public/gpudevice.h
@@ -165,7 +165,8 @@ class GpuDevice : public HWCThread {
   void InitializeMosaicDisplay(
       std::vector<NativeDisplay*>& total_displays_,
       std::vector<std::vector<uint32_t>>& mosaic_displays,
-      std::vector<NativeDisplay*>& temp_displays);
+      std::vector<NativeDisplay*>& temp_displays,
+      std::vector<bool>& available_displays);
   void InitializeCloneDisplay(
       std::vector<NativeDisplay*>& total_displays_,
       std::vector<std::vector<uint32_t>>& cloned_displays);