intel/IA-Hardware-Composer

commit ea1c5e5a1bd4830b7c8b8da230e816e5efda1acb

drm_hwcomposer: Add z order support
Currently, the planner just pops the first available drm plane and if
that can't be used for the DrmHwcLayer it just returns error.

This proposes a slighlty smarter way to do that by trying to see if
any of the DrmPlane can be used for the DrmHwcLayer in question.

More, if the drm_plane doesn't have a fix zorder then we could re-add
him to the list of unused planes, so it could be used for hosting
other less demanding DrmHwcLayers.

Signed-off-by: Alexandru Gheorghe <alexandru-cosmin.gheorghe@arm.com>

drmdisplaycompositor.cpp
@@ -359,6 +359,17 @@ int DrmDisplayCompositor::CommitFrame(DrmDisplayComposition *display_comp,
         }
       }
 
+      if (plane->zpos_property().id() && !plane->zpos_property().immutable()) {
+        ret = drmModeAtomicAddProperty(pset, plane->id(),
+                                       plane->zpos_property().id(),
+                                       source_layers.front()) < 0;
+        if (ret) {
+          ALOGE("Failed to add zpos property %d to plane %d",
+                plane->zpos_property().id(), plane->id());
+          break;
+        }
+      }
+
       rotation = 0;
       if (layer.transform & DrmHwcTransform::kFlipH)
         rotation |= DRM_MODE_REFLECT_X;

drmproperty.cpp
@@ -100,6 +100,10 @@ int DrmProperty::value(uint64_t *value) const {
   }
 }
 
+bool DrmProperty::immutable() const {
+  return id_ && (flags_ & DRM_MODE_PROP_IMMUTABLE);
+}
+
 std::tuple<uint64_t, int> DrmProperty::GetEnumValueWithName(
     std::string name) const {
   for (auto it : enums_) {

drmproperty.h
@@ -46,6 +46,7 @@ class DrmProperty {
   std::string name() const;
 
   int value(uint64_t *value) const;
+  bool immutable() const;
 
  private:
   class DrmPropertyEnum {

drmplane.cpp
@@ -118,6 +118,10 @@ int DrmPlane::Init() {
     return ret;
   }
 
+  ret = drm_->GetPlaneProperty(*this, "zpos", &zpos_property_);
+  if (ret)
+    ALOGE("Could not get zpos property for plane %u", id());
+
   ret = drm_->GetPlaneProperty(*this, "rotation", &rotation_property_);
   if (ret)
     ALOGE("Could not get rotation property");
@@ -187,6 +191,10 @@ const DrmProperty &DrmPlane::src_w_property() const {
 
 const DrmProperty &DrmPlane::src_h_property() const {
   return src_h_property_;
+}
+
+const DrmProperty &DrmPlane::zpos_property() const {
+  return zpos_property_;
 }
 
 const DrmProperty &DrmPlane::rotation_property() const {

drmplane.h
@@ -52,6 +52,7 @@ class DrmPlane {
   const DrmProperty &src_y_property() const;
   const DrmProperty &src_w_property() const;
   const DrmProperty &src_h_property() const;
+  const DrmProperty &zpos_property() const;
   const DrmProperty &rotation_property() const;
   const DrmProperty &alpha_property() const;
   const DrmProperty &blend_property() const;
@@ -75,6 +76,7 @@ class DrmPlane {
   DrmProperty src_y_property_;
   DrmProperty src_w_property_;
   DrmProperty src_h_property_;
+  DrmProperty zpos_property_;
   DrmProperty rotation_property_;
   DrmProperty alpha_property_;
   DrmProperty blend_property_;

platform.h
@@ -81,16 +81,24 @@ class Planner {
                        DrmCompositionPlane::Type type, DrmCrtc *crtc,
                        std::pair<size_t, DrmHwcLayer *> layer) {
       DrmPlane *plane = PopPlane(planes);
-      int ret;
-      if (!plane)
-        return -ENOENT;
+      std::vector<DrmPlane *> unused_planes;
+      int ret = -ENOENT;
+      while (plane) {
+        ret = ValidatePlane(plane, layer.second);
+        if (!ret)
+          break;
+        if (!plane->zpos_property().immutable())
+          unused_planes.push_back(plane);
+        plane = PopPlane(planes);
+      }
 
-      ret = ValidatePlane(plane, layer.second);
-      if (ret)
-        return -EINVAL;
+      if (!ret) {
+        composition->emplace_back(type, plane, crtc, layer.first);
+        planes->insert(planes->begin(), unused_planes.begin(),
+                       unused_planes.end());
+      }
 
-      composition->emplace_back(type, plane, crtc, layer.first);
-      return 0;
+      return ret;
     }
   };
 

commit 2ed463c2fdb57657378743a44129b1e4b4db0656

drm_hwcomposer: Set zpos relative to the minimum possible value
Current implementation doesn't handle properly the cases where zpos
range starts from 1.
See https://gitlab.freedesktop.org/drm-hwcomposer/drm-hwcomposer/issues/19#note_100622

Fixes: ea1c5e5a ("drm_hwcomposer: Add z order support")

Signed-off-by: Alexandru Gheorghe <alexc.g1.ro@gmail.com>
[seanpaul converted to std::tuple return type]
Signed-off-by: Sean Paul <seanpaul@chromium.org>

Change-Id: I35dc2c1cfd0e38ca3a47cf4e668eeb5f3c470ddb

drmdisplaycompositor.cpp
@@ -361,9 +361,14 @@ int DrmDisplayCompositor::CommitFrame(DrmDisplayComposition *display_comp,
 
       if (plane->zpos_property().id() &&
           !plane->zpos_property().is_immutable()) {
+        uint64_t min_zpos = 0;
+
+        // Ignore ret and use min_zpos as 0 by default
+        std::tie(std::ignore, min_zpos) = plane->zpos_property().range_min();
+
         ret = drmModeAtomicAddProperty(pset, plane->id(),
                                        plane->zpos_property().id(),
-                                       source_layers.front()) < 0;
+                                       source_layers.front() + min_zpos) < 0;
         if (ret) {
           ALOGE("Failed to add zpos property %d to plane %d",
                 plane->zpos_property().id(), plane->id());

drmproperty.cpp
@@ -99,6 +99,28 @@ bool DrmProperty::is_immutable() const {
   return id_ && (flags_ & DRM_MODE_PROP_IMMUTABLE);
 }
 
+bool DrmProperty::is_range() const {
+  return id_ && (flags_ & DRM_MODE_PROP_RANGE);
+}
+
+std::tuple<int, uint64_t> DrmProperty::range_min() const {
+  if (!is_range())
+    return std::make_tuple(-EINVAL, 0);
+  if (values_.size() < 1)
+    return std::make_tuple(-ENOENT, 0);
+
+  return std::make_tuple(0, values_[0]);
+}
+
+std::tuple<int, uint64_t> DrmProperty::range_max() const {
+  if (!is_range())
+    return std::make_tuple(-EINVAL, 0);
+  if (values_.size() < 2)
+    return std::make_tuple(-ENOENT, 0);
+
+  return std::make_tuple(0, values_[1]);
+}
+
 std::tuple<uint64_t, int> DrmProperty::GetEnumValueWithName(
     std::string name) const {
   for (auto it : enums_) {

drmproperty.h
@@ -48,6 +48,10 @@ class DrmProperty {
   std::tuple<int, uint64_t> value() const;
   bool is_immutable() const;
 
+  bool is_range() const;
+  std::tuple<int, uint64_t> range_min() const;
+  std::tuple<int, uint64_t> range_max() const;
+
  private:
   class DrmPropertyEnum {
    public: