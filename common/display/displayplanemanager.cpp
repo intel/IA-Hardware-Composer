d79a302d40b68bdae989346e9877d58bb37d523a
Wan Shuang <shuang.wan@intel.com>
2019-06-04 18:53:02 +0800

    Revert 2 black stripe related patches
    Revert "Fixed the external display hang during video playback."
    Revert "Fixed the black stripe issue during video playback control bar popup"
    
    Change-Id: I2638468a04518ba9f45209eb5a8486a8de86b99d
    Tests: Video playback with 2 displays and check plane & layer binding.
    Tracked-On: None
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>

--- common/display/displayplanemanager.cpp
+++ common/display/displayplanemanager.cpp
@@ -238,19 +238,8 @@ bool DisplayPlaneManager::ValidateLayers(
               validate_final_layers = !(last_plane.GetOffScreenTarget());
 
             ResetPlaneTarget(last_plane, commit_planes.back());
-          } else {
-            // Plane separation is needed
-            if (!layer->IsSolidColor() && !layer->IsVideoLayer()) {
-              // validate_final_layers needs to be updated to ensure
-              // plane squashing works as expected.
-              if (!validate_final_layers)
-                validate_final_layers = !(last_plane.GetOffScreenTarget());
-              // Current layer is not the video layer and not the solid
-              // color layer as well. Initialize the DisplayPlaneState
-              // object and link it with OverlayPlane object.
-              ResetPlaneTarget(last_plane, commit_planes.back());
-            }
           }
+
           break;
         } else {
           if (composition.empty()) {
